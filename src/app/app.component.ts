61c9edef9323c3fdb2b1e4a79efde2ed9d8ccc13
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-09-24 16:56:24 +0100

    Started homepage with project images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   MainTitle = 'Alfred Portfolio';
+
+  projects = [
+    {
+      title: "Black Hole",
+      image: "/assets/images/black_hole.png"
+    },
+    {
+      title: "Bulb Table",
+      image: "/assets/images/bulb_table2.png"
+    },
+    {
+      title: "Catwalk",
+      image: "/assets/images/catwalk.png"
+    },
+    {
+      title: "Rapier Wand",
+      image: "/assets/images/rapier_wand.png"
+    },
+    {
+      title: "Sofa",
+      image: "/assets/images/sofa_cloth_sim.png"
+    },
+    {
+      title: "Sonic Screwdrivers",
+      image: "/assets/images/sonics.png"
+    },
+    {
+      title: "Wands",
+      image: "/assets/images/wands.png"
+    },
+  ];
+ 
+  ngOnInit() {
+    
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var direction = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.getElementById('main').scrollLeft -= (direction * 80);
+        e.preventDefault();
+    }
+    // IE9, Chrome, Safari, Opera
+    document.getElementById('main').addEventListener('mousewheel', scrollHorizontally, false);
+    // Firefox
+    document.getElementById('main').addEventListener('DOMMouseScroll', scrollHorizontally, false);
+
+  }
 }
